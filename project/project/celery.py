749adfd26f5e3f24adf85d3ba41b83db6d000adf
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-06-13 15:50:47 +0530

    The celery_worker service is working now

--- project/project/celery.py
+++ project/project/celery.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 # TODO: change this in production
 print("Setting up the environment var DJANGO_SETTINGS_MODULE")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.production")
-print("Env var set")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.local")
+print(os.environ.get('DATABASE_URL'))
 
 app = Celery("project")
 
-app.config_from_object("django.config:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
